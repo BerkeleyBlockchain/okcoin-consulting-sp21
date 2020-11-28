fcb6e9a7fb0ff574705fd821f11a0cef3a55c81b
Grace Kull <gracekull21@berkeley.edu>
2020-11-28 15:52:08 -0800

    Work on 0x

--- backend/src/exchanges/ox.js
+++ backend/src/exchanges/ox.js
@@ -1,16 +1,24 @@
+import { ContractWrappers } from '@0x/contract-wrappers';
+
 /**
  * Gets the midprice for the given token pair as well as the inverse midprice.
  * @param tokenFrom An input token of type defined in shared/token.js
  * @param tokenTo An output token of type defined in shared/token.js
  */
-async function getPrices(tokenFrom, tokenTo) {
+async function getPrices(tokenFrom, tokenTo, amtFrom) {
 
-  // TODO
+  const url = "https://kovan.api.0x.org/swap/v1/price?sellToken=" + tokenFrom 
+    + "&buyToken=" + tokenTo + "&sellAmount=" + amtFrom.toString();
 
+  const prices = await fetch(url);
+  const res = await prices.json();
+  const price = res.data[0].price;
+
+  // price does not include slippage
   return {
     exchange: '0x',
-    midprice: '0.0',
-    inverse: '0.0'
+    midprice: price.toSignificant(6),
+    inverse: price.inverse().toSignificant(6)
   };
 
 }
