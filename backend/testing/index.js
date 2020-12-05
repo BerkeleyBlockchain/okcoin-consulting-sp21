8ac0ac3a7cf9c60ad8f136e8af475a4703baea33
Grace Kull <gracekull21@berkeley.edu>
2020-12-05 14:46:06 -0800

    add tests

--- backend/testing/index.js
+++ backend/testing/index.js
@@ -9,17 +9,26 @@ async function runTests() {
     await testUniswap.getPriceTest(TOKENS.DAI, TOKENS.USDC);
     await testUniswap.getPriceTest(TOKENS.DAI, TOKENS.USDT);
     await testUniswap.getPriceTest(TOKENS.USDT, TOKENS.USDC);
+    await testUniswap.getPriceTest(TOKENS.USDT, TOKENS.DAI);
+    await testUniswap.getPriceTest(TOKENS.USDC, TOKENS.USDT);
+    await testUniswap.getPriceTest(TOKENS.USDC, TOKENS.DAI);
 
     /* Kyber Tests */
     await testKyber.getPriceTest(TOKENS.DAI, TOKENS.USDC);
     await testKyber.getPriceTest(TOKENS.DAI, TOKENS.USDT);
     await testKyber.getPriceTest(TOKENS.USDT, TOKENS.USDC);
+    await testKyber.getPriceTest(TOKENS.USDT, TOKENS.DAI);
+    await testKyber.getPriceTest(TOKENS.USDC, TOKENS.USDT);
+    await testKyber.getPriceTest(TOKENS.USDC, TOKENS.DAI);
 
     /* 0x Tests */
 
     await testZeroX.getPriceTest(TOKENS.DAI, TOKENS.USDC);
     await testZeroX.getPriceTest(TOKENS.DAI, TOKENS.USDT);
     await testZeroX.getPriceTest(TOKENS.USDT, TOKENS.USDC);
+    await testZeroX.getPriceTest(TOKENS.USDT, TOKENS.DAI);
+    await testZeroX.getPriceTest(TOKENS.USDC, TOKENS.USDT);
+    await testZeroX.getPriceTest(TOKENS.USDC, TOKENS.DAI);
 
 }
 
