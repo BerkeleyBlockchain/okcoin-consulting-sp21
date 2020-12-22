b37a0376331f70f6460409414c1d2ebaecdb768a
Sehyun Chung <sehyun@berkeley.edu>
2020-12-22 15:04:41 -0500

    fix useUniswapSwap hook

--- frontend/src/hooks/useUniswapSwap.js
+++ frontend/src/hooks/useUniswapSwap.js
@@ -1,30 +1,51 @@
-import { ChainId, TradeType, Fetcher, Percent, Route, Token, TokenAmount, Trade } from '@uniswap/sdk';
+import {
+  ChainId,
+  TradeType,
+  Fetcher,
+  Percent,
+  Route,
+  Token,
+  TokenAmount,
+  Trade,
+} from '@uniswap/sdk';
 import { ethers } from 'ethers';
-import IUniswapV2Router02 from '@uniswap/v2-periphery/build/IUniswapV2Router02.json';
+import IUniswapV2Router02 from '../constants/IUniswapV2Router02.json';
 import erc20Abi from '../constants/erc20abi.json';
 
 export default async function executeSwap(tokenFrom, tokenTo, inputAmount) {
-
   // Get Ethereum provider
-  const provider = new ethers.providers.InfuraProvider("mainnet", {
+  const provider = new ethers.providers.InfuraProvider('mainnet', {
     projectId: process.env.REACT_APP_INFURA_PROJECT_ID,
-    projectSecret: process.env.REACT_APP_INFURA_PROJECT_SECRET
   });
 
   // Get pricing information from the SDK
-  const amountIn = ethers.utils.parseUnits(inputAmount, inputToken.decimals);
   const inputToken = new Token(ChainId.MAINNET, tokenFrom.mainnet, tokenFrom.decimals);
   const outputToken = new Token(ChainId.MAINNET, tokenTo.mainnet, tokenTo.decimals);
+  console.log(
+    '🚀 ~ file: useUniswapSwap.js ~ line 37 ~ executeSwap ~ inputToken.decimals',
+    inputToken.decimals
+  );
+  const amountIn = ethers.utils.parseUnits(inputAmount, inputToken.decimals);
   const pair = await Fetcher.fetchPairData(inputToken, outputToken, provider);
   const route = new Route([pair], inputToken);
-  const trade = new Trade(route, new TokenAmount(inputToken, amountIn.toString()), TradeType.EXACT_INPUT);
+  const trade = new Trade(
+    route,
+    new TokenAmount(inputToken, amountIn.toString()),
+    TradeType.EXACT_INPUT
+  );
 
   // Print pricing info
-  console.log(`${tokenFrom.ticker} to ${tokenTo.ticker} execution price: ${trade.executionPrice.toSignificant(6)}`);
+  console.log(
+    `${tokenFrom.ticker} to ${tokenTo.ticker} execution price: ${trade.executionPrice.toSignificant(
+      6
+    )}`
+  );
 
   // Get other trade parameters
   const slippageTolerance = new Percent('300', '10000'); // 300 bips, or 3%
-  const amountOutMin = ethers.BigNumber.from(trade.minimumAmountOut(slippageTolerance).raw.toString()); // needs to be converted to e.g. hex
+  const amountOutMin = ethers.BigNumber.from(
+    trade.minimumAmountOut(slippageTolerance).raw.toString()
+  ); // needs to be converted to e.g. hex
   const path = [inputToken.address, outputToken.address];
   const to = process.env.REACT_APP_WALLET_ADDRESS;
   const deadline = Math.floor(Date.now() / 1000) + 60 * 20; // 20 minutes from the current Unix time
@@ -41,11 +62,7 @@ export default async function executeSwap(tokenFrom, tokenTo, inputAmount) {
   );
 
   // Construct erc20 contract
-  const erc20Contract = new ethers.Contract(
-    inputToken.address,
-    erc20Abi,
-    account
-  );
+  const erc20Contract = new ethers.Contract(inputToken.address, erc20Abi, account);
 
   // Get gas price from Infura
   const gasPrice = await provider.getGasPrice();
@@ -55,8 +72,8 @@ export default async function executeSwap(tokenFrom, tokenTo, inputAmount) {
     process.env.REACT_APP_UNISWAP_ROUTER_CONTRACT,
     amountIn,
     {
-      gasPrice: gasPrice,
-      gasLimit: ethers.BigNumber.from(300000)
+      gasPrice,
+      gasLimit: ethers.BigNumber.from(300000),
     }
   );
 
@@ -73,13 +90,13 @@ export default async function executeSwap(tokenFrom, tokenTo, inputAmount) {
     to,
     deadline,
     {
-      gasPrice: gasPrice,
-      gasLimit: ethers.BigNumber.from(300000)
+      gasPrice,
+      gasLimit: ethers.BigNumber.from(300000),
     }
   );
 
   console.log(`Swap transaction hash: ${txSwap.hash}`);
-  
+
   const swapReceipt = await txSwap.wait();
   console.log(`Swap transaction was mined in block ${swapReceipt.blockNumber}`);
 }
