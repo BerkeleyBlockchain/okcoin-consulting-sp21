123b9ceabd453d374bd293f37d77a940f77b974e
Sehyun Chung <sehyun@berkeley.edu>
2021-01-10 23:46:28 -0500

    fix merge conflicts in NavBar

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -5,21 +5,9 @@ export default function NavBar({ setTabIndex }) {
   return (
     <Tabs onChange={(index) => setTabIndex(index)}>
       <TabList>
-<<<<<<< HEAD
-<<<<<<< HEAD
         <Tab>Exchanges</Tab>
         <Tab>Price Graphs</Tab>
         <Tab>My Wallet</Tab>
-=======
-        <Tab>One</Tab>
-        <Tab>Two</Tab>
-        <Tab>Three</Tab>
->>>>>>> 7b6cf30 (add tabs)
-=======
-        <Tab>Exchanges</Tab>
-        <Tab>Price Graphs</Tab>
-        <Tab>My Wallet</Tab>
->>>>>>> a9fa563 (add mywallet view)
       </TabList>
     </Tabs>
   );
