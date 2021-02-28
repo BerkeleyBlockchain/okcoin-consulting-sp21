2556a341cbb4ade326ab980e53e546dce81ff12d
Sehyun Chung <sehyun@berkeley.edu>
2021-02-28 14:42:41 -0800

    remove landing page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import DashboardView from './views/DashboardView';
-import LandingPageView from './views/LandingPageView';
 
 function App() {
   return (
     <Switch>
       <Route path="/app" component={DashboardView} />
-      <Route exact path="/" component={LandingPageView} />
       <Route path="*" component={() => <h1>Page not found</h1>} />
     </Switch>
   );
