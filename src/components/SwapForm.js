9e474d4597fcc187e4f7a4d1a02d471d782e591e
Sishir Giri <sis1001@berkeley.edu>
2021-04-04 14:26:00 -0700

    moved files

--- src/components/SwapForm.js
+++ src/components/SwapForm.js
@@ -13,7 +13,6 @@ import {
 } from '@chakra-ui/react';
 
 import debounce from 'debounce';
-import { useAtom } from 'jotai';
 import { useForm } from 'react-hook-form';
 import React, { useEffect, useState } from 'react';
 
@@ -22,9 +21,6 @@ import FullPageSpinner from './FullPageSpinner';
 import zeroXSwap from '../hooks/use0xSwap';
 import use0xPrice from '../hooks/use0xPrice';
 
-import { pricesAtom } from '../utils/atoms';
-import { estimateAllSwapPrices } from '../utils/getSwapPrice';
-
 import Tokens from '../constants/tokens';
 import Toasts from '../constants/toasts';
 
@@ -32,8 +28,6 @@ export default function SwapForm({ web3, wallet, onboard }) {
   const { register, handleSubmit, watch, setValue, errors } = useForm();
   const [isLoading, setIsLoading] = useState();
   const [sellAmount, setSellAmount] = useState();
-  const [loadingPrices, setLoadingPrices] = useState(false);
-  const [, setPrices] = useAtom(pricesAtom);
   const toast = useToast();
 
   const watchTokenIn = watch('tokenIn', '');
@@ -64,26 +58,23 @@ export default function SwapForm({ web3, wallet, onboard }) {
     if (!watchAmountIn) {
       setValue('amountOut', '');
     }
-    if (!watchAmountIn || !watchTokenIn || !watchTokenOut) {
-      setPrices({});
-    }
   }, [price, watchAmountIn, watchTokenIn, watchTokenOut]);
 
-  useEffect(() => {
-    if (!loadingPrices) {
-      setPrices({});
-      if (sellAmount && watchTokenIn && watchTokenOut) {
-        setLoadingPrices(true);
-        estimateAllSwapPrices(watchTokenIn, watchTokenOut, sellAmount).then((values) => {
-          setPrices(values);
-          setLoadingPrices(false);
-        });
-      }
+  async function readyToTransact() {
+    if (!wallet.provider) {
+      const walletSelected = await onboard.walletSelect();
+      if (!walletSelected) return false;
     }
-  }, [sellAmount, watchTokenIn, watchTokenOut]);
+
+    const ready = await onboard.walletCheck();
+    return ready;
+  }
 
   // Execute the swap
-  const onSubmit = (data) => {
+  const onSubmit = async (data) => {
+    const ready = await readyToTransact();
+    if (!ready) return;
+
     const { amountIn, tokenIn, tokenOut } = data;
     setIsLoading(true);
 
--- src/utils/atoms.js
+++ src/utils/atoms.js
@@ -1,27 +1,4 @@
 import { atom } from 'jotai';
 
+// eslint-disable-next-line import/prefer-default-export
 export const tabIndexAtom = atom(0);
-export const pricesAtom = atom({
-  Balancer: null,
-  Bancor: null,
-  CREAM: null,
-  CryptoCom: null,
-  Curve: null,
-  DODO: null,
-  DODO_V2: null,
-  Eth2Dai: null,
-  Kyber: null,
-  Linkswap: null,
-  LiquidityProvider: null,
-  Mooniswap: null,
-  MultiBridge: null,
-  MultiHop: null,
-  Native: null,
-  Shell: null,
-  SnowSwap: null,
-  SushiSwap: null,
-  Swerve: null,
-  Uniswap: null,
-  Uniswap_V2: null,
-  mStable: null,
-});
